f40909fe39f9b4e6a4a60d2d4874bdc6ce480849
beetumaan <b2maan@yahoo.com>
2021-07-06 09:21:02 +0530

    try

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var express = require('express');
 var path = require('path');
 const fs = require("fs");
 const requests = require("requests");
-const { parse } = require("path/posix");
+const {
+    parse
+} = require("path/posix");
 const port = process.env.PORT || 8000;
 const app = express();
-
 app.use(express.static("public"));
 console.log(__dirname);
-
 
 function tocel(temp) {
     var a = parseInt(temp);
@@ -19,27 +19,24 @@ function tocel(temp) {
 }
 app.set('view engine', 'ejs');
 app.get("/", function(req, res) {
-    requests(
-            "http://api.openweathermap.org/data/2.5/weather?q=sikar&appid=2d42aeb3df951886352515d6a43c754b"
-        )
-        .on("data", (chunk) => {
-            const arr = JSON.parse(chunk);
-            const arrdata = [arr];
-            // console.log(tocel(98.7));
-            res.render("index", {
-                location: arrdata[0].name,
-                country: arrdata[0].sys.country,
-                temp: tocel(arrdata[0].main.temp),
-                temp_max: tocel(arrdata[0].main.temp_max),
-                temp_min: tocel(arrdata[0].main.temp_min)
-            });
-        })
-        .on("end", function(err) {
-            if (err) return console.log("connection closed due to errors", err);
-            console.log("end");
-            res.end();
+    requests("http://api.openweathermap.org/data/2.5/weather?q=sikar&appid=2d42aeb3df951886352515d6a43c754b").on("data", (chunk) => {
+        const arr = JSON.parse(chunk);
+        const arrdata = [arr];
+        // console.log(tocel(98.7));
+        res.render("index", {
+            location: arrdata[0].name,
+            country: arrdata[0].sys.country,
+            temp: tocel(arrdata[0].main.temp),
+            temp_max: tocel(arrdata[0].main.temp_max),
+            temp_min: tocel(arrdata[0].main.temp_min)
         });
+    }).on("end", function(err) {
+        if (err) return console.log("connection closed due to errors", err);
+        console.log("end");
+        res.end();
+    });
 })
+
 
 app.listen(port, function() {
     console.log("running");
